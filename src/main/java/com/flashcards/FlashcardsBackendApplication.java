6fecbe823e37a9659c73a47675d8529e0f071db5
Jacob Lista <jvl7071@rit.edu>
2025-07-24 22:00:17 -0400

    switched from MongoDB to Postgres

--- src/main/java/com/flashcards/FlashcardsBackendApplication.java
+++ src/main/java/com/flashcards/FlashcardsBackendApplication.java
@@ -1,13 +1,20 @@
 package com.flashcards;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import com.flashcards.repository.CardRepository;
 
 @SpringBootApplication
-@EnableMongoRepositories(basePackages = "com.flashcards.repository")
+//@EnableJpaRepositories(basePackages = "com.flashcards.repository")
 public class FlashcardsBackendApplication {
   public static void main(String[] args) {
     SpringApplication.run(FlashcardsBackendApplication.class, args);
   }
+
+    @Autowired
+    CardRepository cardRepository;
 }
+
+
