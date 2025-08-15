c3e0695775db1768c5c07b79132f383b7a11d532
Jacob Lista <jvl7071@rit.edu>
2025-08-15 11:26:37 -0400

    updated CORS filters for frontend running on port 8000

--- src/main/java/com/flashcards/config/SecurityConfig.java
+++ src/main/java/com/flashcards/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:8000"));
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE"));
         configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
 
--- src/main/java/com/flashcards/config/WebConfig.java
+++ src/main/java/com/flashcards/config/WebConfig.java
@@ -31,7 +31,7 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000")
+                registry.addMapping("/**").allowedOrigins("http://localhost:8000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*").allowCredentials(true);
             }
